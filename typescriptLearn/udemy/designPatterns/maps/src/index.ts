65ff3dc93a8ad90372c148618508e9431bac2f59
redbugmilk <redbugmilk@gmail.com>
2022-02-07 09:24:20 +0000

    add google maps

---
+++ typescriptLearn/udemy/designPatterns/maps/src/index.ts
@@ -0,0 +1,12 @@
+// import { Company } from './Company';
+// import { User } from "./User";
+
+
+new google.maps.Map(document.getElementById("map"),{
+  zoom: 1,
+  backgroundColor:"pink",
+  center:{
+    lat:0,
+    lng:0
+  }
+})
