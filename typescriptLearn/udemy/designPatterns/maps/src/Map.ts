15d06651e5b610728416d00b8ad7712f8a4bdd21
redbugmilk <redbugmilk@gmail.com>
2022-02-24 21:20:15 +0000

    addInfoToMarker

--- typescriptLearn/udemy/designPatterns/maps/src/Map.ts
+++ typescriptLearn/udemy/designPatterns/maps/src/Map.ts
@@ -21,13 +21,21 @@ export class Map {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map:this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng
       }
-    })
+    });
+    marker.addListener('click',this.addInfoToMarker(marker))
+  }
+
+  addInfoToMarker(marker: google.maps.Marker ){
+    const infoWindow = new google.maps.InfoWindow({
+      content: "hello world"
+    });
+    infoWindow.open(this.googleMap,marker);
   }
 
 }
