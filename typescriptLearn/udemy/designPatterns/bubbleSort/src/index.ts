cd1622c85a69b1d87df232f26daa29d2cf893ad9
redbugmilk <redbugmilk@gmail.com>
2022-05-04 19:19:21 +0100

    bunch of training

--- typescriptLearn/udemy/designPatterns/bubbleSort/src/index.ts
+++ typescriptLearn/udemy/designPatterns/bubbleSort/src/index.ts
@@ -1,1 +1,5 @@
-console.log("ola joana")
+import { NumberCollection } from './NumberCollection';
+const numberCollection = new NumberCollection([50, 23, 9, 18, 61, 32])
+console.log(numberCollection.data)
+numberCollection.sort();
+console.log(numberCollection.data)
